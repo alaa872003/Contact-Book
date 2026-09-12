90162b678d89848dcd2da2bd0ff268cee78f86f1
alaa872003 <alaa.moh.latif@gmail.com>
2026-09-12 04:33:17 +0300

    plan basic functions

---
+++ src/contacts.js
@@ -0,0 +1,24 @@
+// Add Contact
+function addContact(){
+
+}
+
+// Remove Contact
+function removeContact(){
+
+}
+
+// Update Contact
+function updateContact(){
+
+}
+
+// List Contacts
+function listContacts(){
+
+}
+
+// Search Contact
+function searchContact(){
+    
+}
